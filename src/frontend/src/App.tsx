f165c7e059f722f1e361741353f41a0b18d90810
varun789-mx <varunsh153@gmail.com>
2025-01-29 22:38:36 +0530

    Fixed the create react issues it works only on windows

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Tum logo ki ....</h1>
+      <h2>
+        {" "}
+        Hello world
+        <a href="https://www.google.com" target="_blank">
+          Hello world
+        </a>
+      </h2>
+    </>
+  );
+}
+
+export default App;
