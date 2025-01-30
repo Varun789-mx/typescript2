7950ac132a20f5c9ef00a699f791ea6543a1a736
varun789-mx <varunsh153@gmail.com>
2025-01-31 01:02:20 +0530

    small fix

--- src/Backend/middlewares/Signup.ts
+++ src/Backend/middlewares/Signup.ts
@@ -1,2 +1,45 @@
-import express from "express";
-import PORT from "config.js"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { User, new_user } from "../ALL_EXPORTS.js";
+
+const New_user = express();
+
+New_user.use(cors());
+New_user.use(express.json());
+
+interface SignupRequestBody {
+  first_name: string;
+  Last_name: string;
+  email: string;
+  mobile: string;
+  Password: string;
+}
+
+New_user.post("/signup", async (req: Request<unknown, unknown, SignupRequestBody>, res: Response) => {
+  const DATA = {
+    First_name: req.body.first_name,
+    Last_name: req.body.Last_name,
+    Email: req.body.email,
+    Mobile: req.body.mobile,
+    Password: req.body.Password,
+  };
+  
+  const safedata = new_user.safeparse(DATA);
+  
+  if (!safedata.success) {
+    return res.status(411).json({
+      msg: "input fields are incorrect",
+    });
+  }
+  
+  const create_user = await User.findOne({
+    Email:safedata.data.Email,
+    First_name:safedata.data.First_name,
+  });
+
+  if(!create_user) {
+    return res.status(402).json({
+      msg:"user already exist please login",
+    });
+  }
+});
