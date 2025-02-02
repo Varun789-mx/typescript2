dfd36315e172232508b7c8ef49593d34b77c8e10
varun789-mx <varunsh153@gmail.com>
2025-02-02 16:51:33 +0530

    added tailwindcss for frontend

---
+++ src/frontend/src/components/navbar.tsx
@@ -0,0 +1,18 @@
+
+
+export const Navbar = ({}) => {
+  return (
+    <>
+      <nav>
+        <div className="bg-blue-400  text-black w-full h-5">
+          <div>Name</div>
+          <div>Paytm</div>
+          <div>
+            <div>search</div>
+            <div>msg</div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
