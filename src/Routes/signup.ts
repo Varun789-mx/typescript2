cdfb269fff88443c1ddbe1c5797bb62cd6efacfa
Varun789-mx <varunsh153@gmail.com>
2025-02-10 19:17:22 +0000

    fixed the error in signup use json format in postman

--- src/Routes/signup.ts
+++ src/Routes/signup.ts
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import zod from "zod";
 import { getclient } from "../db/config";
-import jwt from "jsonwebtoken";	
+import jwt from "jsonwebtoken";
 import cors from "cors";
 import { status_code } from "./Status_code";
 import { createuser } from "../db/Insertuser";
@@ -10,64 +10,72 @@ const jwtsecret = "samplestring";
 
 
 
- interface user_fields {
-     firstname: string,
-     lastname: string,
-     email: string,
-     mobile: string,
-     password: string,
+  interface user_fields {
+      firstname: string,
+      lastname: string,
+      email: string,
+      mobile: string,
+      password: string,
+  }
+
+
+
+ const signupbody = zod.object({
+     firstname: zod.string(),
+     lastname: zod.string(),
+     email: zod.string(),
+     mobile: zod.string(),
+     password: zod.string()
+ });
+
+
+ async function ExistingUser(email: string) {
+     try {
+         const query = `SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)`;
+         const client = await getclient();
+         if (!client) {
+             throw new Error("failed to get db client");
+         }
+         const result = await client.query(query, [email]);
+         return result.rows[0].exists;
+     }
+     catch (error) {
+         console.error("Error in checking the email", error);
+     }
  }
 
-
-
-const signupbody = zod.object({
-    firstname: zod.string(),
-    lastname: zod.string(),
-    email: zod.string().email(),
-    mobile: zod.string().min(5, "mobile no should be atleast 10 digits long"),
-    password: zod.string().min(8, "password should be atleast 8 characters long")
-});
-
-
-async function ExistingUser(email: string) {
-    try {
-        const query = `SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)`;
-        const client = await getclient();
-        if (!client) {
-            throw new Error("failed to get db client");
-        }
-        const result = await client.query(query, [email]);
-        return result.rows[0].exists;
-    }
-    catch (error) {
-        console.error("Error in checking the email", error);
-    }
-}
-
-userRouter.post('/signup', async (req,res):Promise<any>=> {
-    try {
-        const data  = signupbody.safeParse(req.body);
-        if (!data.success) {
-            return res.status(status_code.Success).json({
-                msg: "Incorrect inputs",
-                error: data.error,
-            });
-        }
-        const prevuser = await ExistingUser(req.body.email);
-        if(!prevuser) {
-            return res.status(status_code.Bad_Request).json({
-                message:"User already exists",
-            });
-        }
-	const adduser = await createuser(req.body);
-	const token = await jwt.sign({email: req.body.email},jwtsecret);
-	return res.status(status_code.Success).json({
-	msg:"user succesfully created",
-	token:token 
-	});
-    } catch (error) {
-        console.log("error occured while signup", error);
-    }
-});
+ userRouter.post('/signup', async (req,res):Promise<any>=> {
+     try {
+	  const ruff:user_fields = {
+	  firstname:req.body.firstname,
+	  lastname:req.body.lastname,
+	  email:req.body.email,
+	  mobile:req.body.mobile,
+	  password:req.body.password
+	  };
+         const data  = signupbody.safeParse(ruff);
+	 console.log(ruff);
+         if (!data.success) {
+             return res.status(status_code.Success).json({
+                 msg: "Incorrect inputs",
+                 error: data.error.issues
+             });
+         }
+         const prevuser = await ExistingUser(req.body.email);
+         if(prevuser) {
+             return res.status(status_code.Bad_Request).json({
+                 message:"User already exists",
+             });
+         }
+         const adduser = await createuser(req.body);
+         const token = await jwt.sign({email: req.body.email},jwtsecret);
+         return res.status(status_code.Success).json({
+         msg:"user succesfully created",
+         token:token
+         });
+     } catch (error) {
+         console.log("error occured while signup", error);
+     }
+ });
 
 
