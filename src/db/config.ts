99ccbdf6d39da9a71b71ea7934893d6b69aa728a
Varun789-mx <varunsh153@gmail.com>
2025-02-11 20:17:34 +0000

    fixed the issue in Usertable.ts

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,12 +1,13 @@
-import {Client} from "pg";
+import {Client} from "pg"
 
 export async function getclient() { 
-  const client = new Client({connectionString:`postgresql://postgres:harsh@localhost:5431/postgres`});
-	  try{
-	   await client.connect();
-	   console.log("Connection success");
-	   return client;
-	   }catch(error) {
-		   console.log("Error occured in db connection",error);
-	   }
+	const client = new Client({ connectionString:'postgresql://postgres:harsh@localhost:5431/postgres'});
+	try { 
+		await client.connect();
+		console.log("Connection to db is succesfull");
+	}
+	catch(error) { 
+		console.log("Error occured in connection " + error);
+	}
+	return client;
 }
