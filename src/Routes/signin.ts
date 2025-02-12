7b4954b1b92afbabe4479edc8ea3327cccd94362
varun789-mx <varunsh153@gmail.com>
2025-02-13 01:33:42 +0530

    fixed some basic error

--- src/Routes/signin.ts
+++ src/Routes/signin.ts
@@ -1,58 +1,57 @@
-import {Router} from "express";
+import { Router } from "express";
 import zod from "zod";
 import bcrypt from "bcrypt";
-import {getclient} from "../db/config";
+import { getclient } from "../db/config";
 import jwt from "jsonwebtoken";
 import { status_code } from "./Status_code";
 
 
 export const signinroute = Router();
 
-const signinbody = zod.object({ 
-	email:zod.string().email(),
-	password:zod.string()
+const signinbody = zod.object({
+	email: zod.string().email(),
+	password: zod.string()
 });
-async function Checkuser(email:string,password:string) { 
+async function Checkuser(email: string, password: string) {
 	const hash = finduser(email);
-	const match = await bcrypt.compare(password,hash);
-	if(match) { 
+	const match = await bcrypt.compare(password, hash);
+	if (match) {
 		return true;
-	} 
+	}
 	return false;
 }
-async function finduser(email:string) { 
-	const client = getclient(); 
+async function finduser(email: string) {
+	const client = getclient();
 	const findquery = `SELECT * FROM paytmuser WHERE email = $1 RETURNING password`;
 	try {
-       	const findres = await client.query(findquery,email);
-	return findres;
+		const findres = await (await client).query(findquery, [email]);
+		return findres;
 	}
-	catch(error) { 
+	catch (error) {
 		console.error("User doesn't exist please signup ");
+	}
 }
-}
-
-signin.post('/signin',async (req,res):Promise<any>=> {
-       try{ 
-       	   const {email,password} = req.body;
-           const safedata = signinbody.safeParse({email,password});
-		if(!safedata.success) { 
-	    		return res.status(Bad_request).json({
-				msg:"Incorrect inputs",
-				error:safedata.error
+signinroute.post('/signin', (req, res) => {
+	try {
+		const { email, password } = req.body;
+		const safedata = signinbody.safeParse({ email, password });
+		if (!safedata.success) {
+			return res.status(status_code.Bad_Request).json({
+				msg: "Incorrect inputs",
+				error: safedata.error
 			});
 		}
-	  const verifyuser = Checkuser(safedata.email,safedata.password);
-	  if(!verify) { 
-		  return res.status(Status_code.Bad_request).json({
-			  msg:"Incorrect password"
-		  });
-	  }
-       }
-       catch(error) { 
-	       res.status(Stauts_code.Request_timeout).json({
-		       msg:"Request failed"
-	       });
-       }
+		const verifyuser = Checkuser(safedata.data.email, safedata.data.password);
+		if (!verifyuser) {
+			return res.status(status_code.Bad_Request).json({
+				msg: "Incorrect password"
+			});
+		}
+	}
+	catch (error) {
+		return res.status(status_code.Request_timeout).json({
+			msg: "Request failed"
+		});
+	}
 }
 );
