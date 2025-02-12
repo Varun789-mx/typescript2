7d88beebb66ff92acbd33caaa45bf34cacac4bd8
Varun789-mx <varunsh153@gmail.com>
2025-02-12 19:45:27 +0000

    minor progress in signin

--- src/Routes/signin.ts
+++ src/Routes/signin.ts
@@ -1,7 +1,8 @@
-import {Router} from "express"
-import zod from "zod" 
+import {Router} from "express";
+import zod from "zod";
+import bcrypt from "bcrypt";
 import {getclient} from "../db/config";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { status_code } from "./Status_code";
 
 
@@ -9,20 +10,27 @@ export const signinroute = Router();
 
 const signinbody = zod.object({ 
 	email:zod.string().email(),
-	passoword:zod.string()
+	password:zod.string()
 });
-async function Checkuser(email,password) { 
+async function Checkuser(email:string,password:string) { 
+	const hash = finduser(email);
 	const match = await bcrypt.compare(password,hash);
 	if(match) { 
 		return true;
 	} 
 	return false;
 }
-async function finduser(email) { 
+async function finduser(email:string) { 
 	const client = getclient(); 
-	const findquery = `SELECT * FROM paytmuser WHERE 
-
-
+	const findquery = `SELECT * FROM paytmuser WHERE email = $1 RETURNING password`;
+	try {
+       	const findres = await client.query(findquery,email);
+	return findres;
+	}
+	catch(error) { 
+		console.error("User doesn't exist please signup ");
+}
+}
 
 signin.post('/signin',async (req,res):Promise<any>=> {
        try{ 
@@ -34,5 +42,17 @@ signin.post('/signin',async (req,res):Promise<any>=> {
 				error:safedata.error
 			});
 		}
-	  
-
+	  const verifyuser = Checkuser(safedata.email,safedata.password);
+	  if(!verify) { 
+		  return res.status(Status_code.Bad_request).json({
+			  msg:"Incorrect password"
+		  });
+	  }
+       }
+       catch(error) { 
+	       res.status(Stauts_code.Request_timeout).json({
+		       msg:"Request failed"
+	       });
+       }
+}
+);
